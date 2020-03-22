068dc0e4d75809637cb5754d0e55d782fcf9b2b9
calhargis <57971547+calhargis@users.noreply.github.com>
2020-03-21 21:38:34 -0600

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1 class="center">Everything you need to know:</h1>
+    <p>hello</p>
+    <li>Survived car crash</li>
+    <li>Survived Appendicitis</li>
+
+    <h2 class="center">Dimensions:</h2>
+    <img class="center" src="https://dw3jhbqsbya58.cloudfront.net/rosters/6991b226-69a6-439a-a072-e63cd4325aab/c/7/f/c7f57d1c-d2c4-e711-93f8-ecf4bbe7be14/thumbnail.jpg?version=636845464623148690" alt="Jacob" height="250" width="250">
+    <li>6'3"</li>
+    <li>160</li>
+
+  </div>
+</template>
+
+<style scoped>
+
+.center {
+  text-align: center;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Jacob" src="https://dw3jhbqsbya58.cloudfront.net/rosters/cfeda153-cbfc-46c2-95e1-8aff022efc11/7/f/5/7f53db19-1bb0-e611-8076-a0369f3c18ea/thumbnail.jpg?version=636845464623148690">
+    <h1>This is Jacob Fox</h1>
+    <p>The man</p>
+    <p>The myth</p>
+    <p>The legend</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
